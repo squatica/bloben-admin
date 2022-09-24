c476872bbab1466c4d5a4142c90e489af00e2c43
nibdo <hello@nibdo.com>
2022-09-24 23:03:45 +0200

    Add theme

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import {
   ChakraProvider,
   ColorModeProvider,
   ColorModeScript,
+  ComponentStyleConfig,
+  extendTheme,
 } from '@chakra-ui/react';
 import AuthProvider from './layers/AuthProvider';
 import BrowserProvider from './layers/BrowserProvider';
@@ -13,10 +15,47 @@ import StoreProvider from './context/store';
 // @ts-ignore
 window.env = {};
 
+const Input: ComponentStyleConfig = {
+  defaultProps: {
+    size: 'lg',
+  },
+};
+
+const Button: ComponentStyleConfig = {
+  baseStyle: {
+    fontWeight: 'semibold',
+    borderRadius: 'base',
+    _focus: { boxShadow: 'none' },
+  },
+  defaultProps: {
+    size: 'md',
+    variant: 'solid',
+    _focus: { boxShadow: 'none' },
+  },
+};
+
+const theme = extendTheme({
+  components: {
+    Button,
+    Input,
+    CheckBox: {
+      defaultProps: {
+        _focus: { boxShadow: 'none' },
+      },
+    },
+  },
+  colors: {
+    primary: {
+      200: '#EC407AB2',
+      400: '#ec407a',
+    },
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <ColorModeScript initialColorMode={'light'} />
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <ColorModeProvider options={{ initialColorMode: 'light' }}>
         <StoreProvider>
           <BrowserProvider>
