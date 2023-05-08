a76abe0f0d0f864f634dd57b323d1523defbfe02
squatica <53292272+squatica@users.noreply.github.com>
2023-05-08 11:36:21 +0200

    location.host instead of location.hostname

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -2,6 +2,6 @@ export const getHostname = () => {
   if (import.meta.env.VITE_APP_NODE_ENV === 'development') {
     return 'http://localhost:8080';
   } else {
-    return `${window.location.protocol}//${window.location.hostname}`;
+    return `${window.location.protocol}//${window.location.host}`;
   }
 };
